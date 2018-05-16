0c82ba352645599fa0e2347572233340a95d443d
MrQuJL <909301740@qq.com>
2018-05-16 15:32:44 +0800

    add comment of double stack sort

--- 队列和栈/05_双栈排序.java
+++ 队列和栈/05_双栈排序.java
@@ -1,5 +1,12 @@
 import java.util.*;
 
+/**
+ * 题目名称：双栈排序
+ * 题目描述：请编写一个程序，按升序对栈进行排序（即最大元素位于栈顶），要求最多只能使用一个额外的栈存放临时数据，
+ * 但不得将元素复制到别的数据结构中。
+ * 给定一个int[] numbers(C++中为vector&ltint>)，其中第一个元素为栈顶，请返回排序后的栈。
+ * 请注意这是一个栈，意味着排序过程中你只能访问到第一个元素。
+ */
 public class TwoStacks {
     public ArrayList<Integer> twoStacksSort(int[] numbers) {
         // 合法性判断
