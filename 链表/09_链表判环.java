b38478fbbe53375bf09d327804a8ab8968cc7411
MrQuJL <909301740@qq.com>
2018-05-23 09:00:45 +0800

    链表判环：快慢指针

--- 链表/09_链表判环.java
+++ 链表/09_链表判环.java
@@ -9,6 +9,13 @@ public class ListNode {
         this.val = val;
     }
 }*/
+/**
+ * 题目名称：链表判环
+ * 题目描述：
+ * 如何判断一个单链表是否有环？有环的话返回进入环的第一个节点的值，无环的话返回-1。
+ * 如果链表的长度为N，请做到时间复杂度O(N)，额外空间复杂度O(1)。
+ * 给定一个单链表的头结点head（注意另一个参数adjust为加密后的数据调整参数，方便数据设置，与本题求解无关)，请返回所求值。
+ */
 public class ChkLoop {
     public int chkLoop(ListNode head, int adjust) {
         // 合法性判断
